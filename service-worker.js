c2ee7bfb4dbbc4e6c34df8792d2644b067769e5e
AHHM0699 <ahhm0699@users.noreply.github.com>
2026-01-26 21:51:11 -0500

    Grupo Montenegro WebApp - Portal de empresas

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'grupo-montenegro-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './images/logo-holding.png',
+    './images/logo-liliana.png',
+    './images/logo-odoncito.png',
+    './images/logo-cheplas.png',
+    './images/logo-entregas.png',
+    './images/logo-pagina.png',
+    './images/logo-ajustes.png'
+];
+
+// Instalar Service Worker
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Cache abierto');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Activar Service Worker
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function(cacheName) {
+                    return cacheName !== CACHE_NAME;
+                }).map(function(cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+// Interceptar peticiones
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
